a6d2cc0555ad45aa3fa0e20f1cea4ffdeaed21ce
charley2017 <charley.millard@ignitec.com>
2016-11-29 17:45:03 +0000

    Create Graph1SecondVal.cs

---
+++ Graph1SecondVal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_test_6
+{
+    public class Graph1SecondVal
+    {
+        public Stopwatch DateTime { get; set; }
+        public double Value { get; set; }
+    }
+}
