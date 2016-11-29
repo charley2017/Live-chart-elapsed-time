152e4c1e2a82ce662f2ea75f7cafc97659962868
charley2017 <charley.millard@ignitec.com>
2016-11-29 17:44:37 +0000

    Create MeasureModel.cs

---
+++ MeasureModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_test_6
+{
+    public class MeasureModel
+    {
+        public Stopwatch DateTime { get; set; }
+        public double Value { get; set; }
+    }
+}
